ee1d71aeed611e63708b08e797fff09c57f6d1d4
linarakstyte <94565077+linarakstyte@users.noreply.github.com>
2021-12-08 14:27:58 +0200

    Naujas projektas

---
+++ BilietavimoSistema/BilietavimoSistema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BilietavimoSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
