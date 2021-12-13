188be53afea80b609137c6a7a872a8d7273c8e5d
linarakstyte <94565077+linarakstyte@users.noreply.github.com>
2021-12-13 10:23:41 +0200

    Pabaigta

---
+++ BilietavimoSistema/BilietavimoSistema/BilietuAtaskaita.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilietavimoSistema
+{
+    class BilietuAtaskaita
+    {
+
+        int _Bilietai10 = 0;
+        int _Bilietai20 = 0;
+        int _Bilietai30 = 0;
+        int _VisoSukurta10 = 0;
+        int _VisoSukurta20 = 0;
+        int _VisoSukurta30 = 0;
+        int _VisoParduota10 = 0;
+        int _VisoParduota20 = 0;
+        int _VisoParduota30 = 0;
+
+        public void KurtiBilietus(int bil10, int bil20, int bil30)
+        {
+            _Bilietai10 += bil10;
+            _Bilietai20 += bil20;
+            _Bilietai30 += bil30;
+            _VisoSukurta10 += bil10;
+            _VisoSukurta20 += bil20;
+            _VisoSukurta30 += bil30;
+            Console.WriteLine($"Siuo metu yra {_Bilietai10} bilietai/u po 10 euru, {_Bilietai20} po 20 euru ir {_Bilietai30} po 30 euru");
+        }
+
+        public void PirktiBilietus(int bil10, int bil20, int bil30)
+        {
+            if (bil10 > _Bilietai10)
+            { Console.WriteLine($"Siuo metu nera pakankamai {_Bilietai10} bilietu po 10 euru parduoti"); }
+            else
+            {
+                _Bilietai10 -= bil10;
+                _VisoParduota10 += bil10;
+            }
+            if (bil20 > _Bilietai20)
+            { Console.WriteLine($"Siuo metu nera pakankamai {_Bilietai20} bilietu po 20 euru parduoti"); }
+            else
+            {
+                _Bilietai20 -= bil20;
+                _VisoParduota20 += bil20;
+            }
+            if (bil30 > _Bilietai30)
+            { Console.WriteLine($"Siuo metu nera pakankamai {_Bilietai30} bilietu po 30 euru parduoti"); }
+            else
+            {
+                _Bilietai30 -= bil30;
+                _VisoParduota30 += bil30;
+            }
+            Console.WriteLine($"Siuo metu yra {_Bilietai10} bieltai/u po 10 euru, {_Bilietai20} po 20 euru ir {_Bilietai30} po 30 euru");
+        }
+
+        public void Ataskaita()
+        {
+            Console.WriteLine($"Is viso sukurta {_VisoSukurta10} bilietai/u po 10 euru, {_VisoSukurta20} po 20 euru ir {_VisoSukurta30} po 30 euru");
+            Console.WriteLine($"Is viso parduoda {_VisoParduota10} bilietai/u po 10 euru, {_VisoParduota20} po 20 euru ir {_VisoParduota30} po 30 euru");
+
+
+        }
+    }
+}
--- BilietavimoSistema/BilietavimoSistema/Program.cs
+++ BilietavimoSistema/BilietavimoSistema/Program.cs
@@ -4,11 +4,51 @@ namespace BilietavimoSistema
 {
     class Program
     {
+                   
         static void Main(string[] args)
         {
-            while True:
-            Console.WriteLine("Meniu: 1 pirkti bilieta, 2 kurti bilieta");
-            string pasirinkimas = (Readline);
+            BilietuAtaskaita ataskaita = new BilietuAtaskaita();
+
+
+            while (true)
+                    {
+                Console.WriteLine("Meniu: spauskite 1 pirkti bilieta/us, 2 kurti bilieta/us, 3 gauti ataskaita ir 0 baigti programa");
+                string pasirinkimas = Console.ReadLine();
+                if (pasirinkimas == "1")
+                {
+                    Console.Write("Iveskite skaicius kiek norite pirkti bilietu ");
+                    Console.Write("po 10 euru:");
+                    int bil10 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("po 20 euru:");
+                    int bil20 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("po 30 euru:");
+                    int bil30 = Convert.ToInt32(Console.ReadLine());
+                    ataskaita.PirktiBilietus(bil10, bil20, bil30);
+
+                }
+                else if (pasirinkimas == "2")
+                {
+                    Console.Write("Iveskite skaicius kiek norite sukurti bilietu ");
+                    Console.Write("po 10 euru:");
+                    int bil10 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("po 20 euru:");
+                    int bil20 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("po 30 euru:");
+                    int bil30 = Convert.ToInt32(Console.ReadLine());
+                    ataskaita.KurtiBilietus(bil10, bil20, bil30);
+                }
+                else if (pasirinkimas == "3")
+                {
+                    ataskaita.Ataskaita();
+                }
+                else if (pasirinkimas == "0")
+                {
+                    Console.Write("Viso gero! ");
+                    break; }
+
+
+
+            }
         }
     }
 }
